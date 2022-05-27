9f2f2cdd14371fb0b1a6d7ad101364d2679c162a
Stefanene <stefanene@hotmail.com>
2022-05-26 20:33:11 -0700

    initial changes to implement use of Python dictionaires as hash map

--- Python/WordleHelp/WordleHelper.py
+++ Python/WordleHelp/WordleHelper.py
@@ -1,3 +1,5 @@
+from tkinter.tix import INTEGER
+from tokenize import String
 import WordChecker
 
 class WordleHelper:
@@ -8,12 +10,23 @@ class WordleHelper:
     # _wordleWordsMap: database of WORDLE words to be evaluated
     #               for extracting proposals matching all constraints.
 
+
     def __init__(self):
         self._wordleChecker = WordChecker.WordChecker()
-        self._wordleWordsMap = {}
+        self._wordleWordsMap = dict(); #use python dictionaries as hash maps
+        self.coutner = 0; #word counter for dictionary
 
     def cmdAdd(self, args):
         print(f"cmdAdd {list(args)}")
+        #adds one word at a time
+        if(args.length != 1):
+            print(f"Input error")
+        word = args.lower();
+        #word checker to make sure it is five letters
+        _wordleWordsMap[counter] = word;
+        counter = counter + 1; #increment word counter
+        print(f"Added {word.upper()} to the words list!")
+
 
     def cmdLoad(self, args):
         print(f"cmdLoad {list(args)}")
@@ -26,3 +39,5 @@ class WordleHelper:
 
     def cmdClear(self, args):
         print(f"cmdClear {list(args)}")
+        counter = 0; #reset word counter
+
