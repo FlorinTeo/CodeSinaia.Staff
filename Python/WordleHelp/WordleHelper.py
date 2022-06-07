0cc2222edb90c9c7d92349bbe2505a58c23e0530
Florin.Teo <Florin.Teo@live.com>
2022-06-06 23:53:32 -0700

    Add scraping code and update dictionary.

--- Python/WordleHelp/WordleHelper.py
+++ Python/WordleHelp/WordleHelper.py
@@ -1,5 +1,6 @@
 import WordChecker
 import os.path
+import requests
 
 class WordleHelper:
     ###
@@ -51,16 +52,19 @@ class WordleHelper:
         if len(args) != 1:
             raise Exception(f"Missing or invalid argument '{args}'. Expected a word or a URL!")
         
-        word = args[0].upper()
-        if not WordleHelper.isWordleWord(word):
-            raise Exception(f"Invalid format for word '{word}'. Expected a WORDLE proper word!")
-
-        #word checker to make sure it is five letters
-        wordCount = 1
-        if word in self._wordleWordsMap:
-            wordCount = self._wordleWordsMap[word] + 1
-        self._wordleWordsMap[word] = wordCount
-        print(f"Added {word}:{self._wordleWordsMap[word]} to the words list!")
+        page = requests.get(args[0])
+        nNewWordleWords = 0
+        for line in page.text.splitlines():
+            for word in line.split():
+                word = word.upper()
+                if WordleHelper.isWordleWord(word):
+                    wordCount = 1
+                    if word in self._wordleWordsMap:
+                        wordCount = self._wordleWordsMap[word] + 1
+                    else:
+                        nNewWordleWords += 1
+                    self._wordleWordsMap[word] = wordCount
+        print(f"Added {nNewWordleWords} to the words list!")
 
     ###
     # Command handler for saving the content of the WordleWordsMap to a text file.
