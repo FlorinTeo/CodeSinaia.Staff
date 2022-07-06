481e842d039ce97b261c929b715c72616bd90e13
Florin.Teo <Florin.Teo@live.com>
2022-07-06 15:57:15 -0700

    Wire in MadLibs

--- Python/Labs/MadLibs/Program.py
+++ Python/Labs/MadLibs/Program.py
@@ -9,7 +9,16 @@ filePath = sys.argv[1]
 if not os.path.isfile(filePath):
     raise Exception(f"File {filePath} cannot be located!")
 
-text = open(filePath)
-for line in text:
+inputText = open(filePath)
+outputText = ''
+for line in inputText:
     words = line.split()
-    print(words)
+    for word in words:
+        suffix = ""
+        if word.startswith("<"):
+            suffix = word[word.index('>')+1:]
+            word = input(f"{word[1:word.index('>')]}?> ")
+        outputText += f" {word}{suffix}"
+    outputText += "\n"
+print()
+print(outputText)
