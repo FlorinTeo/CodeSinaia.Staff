8f2e1ab6657a7c6d21d3b9dfd67cd76634f4e439
Florin.Teo <Florin.Teo@live.com>
2022-07-14 16:10:55 +0200

    Add MadLibs_4

---
+++ Python/Labs/MadLibs/MadLibs_4.py
@@ -0,0 +1,67 @@
+import os
+import sys
+
+class MadLibs:
+    ###
+    # Global variables keeping track of
+    # the words being read fron the file
+    #_words, _iRow, _iCol, _outputText
+
+    ###
+    # Given an input file, it initializes the
+    # state variables with the content read from the file
+    def __init__(self, filePath):
+        if not os.path.isfile(filePath):
+            raise Exception(f"File {filePath} cannot be located!")
+
+        # read the content into an array of strings each standing
+        # for one line of text
+        lines = open(filePath).readlines()
+
+        # break the content further into a global two dimensional jagged array
+        # of words. Each row is an array of words on that corresponding line.
+        self._words = [[] for r in range(len(lines))]
+        for r in range(len(lines)):
+            # split the array around spaces and other delimiters
+            self._words[r] = lines[r].split()
+
+        # initialize the state variables. We start from
+        # first row and first column, with an empty output text
+        self._iRow = 0
+        self._iCol = 0
+        self._outputText = ""
+
+    ###
+    # Extracts the next tagged word and updates the global variables
+    def nextTag(self):
+        while self._iRow < len(self._words):
+            while self._iCol < len(self._words[self._iRow]):
+                word = self._words[self._iRow][self._iCol]
+                self._iCol += 1
+                if word.startswith("<"):
+                    return word
+                self._outputText += f"{word} "
+            self._outputText += "\n"
+            self._iRow += 1
+            self._iCol = 0
+        return None
+
+    ###
+    # Adds the given replacement word to the output
+    def addToOutput(self, replacement):
+        self._outputText
+        self._outputText += f"{replacement} "
+
+###
+# Tests the PlayMadLibs function on the simplest MadLibs text
+if __name__ == "__main__":
+    # print the resulting text
+    madLibs = MadLibs("Python\Labs\MadLibs\input\simple.txt")
+    tag = madLibs.nextTag()
+    while tag != None:
+        iStart = tag.find("<")
+        iEnd = tag.find(">")
+        word = input(f"{tag[iStart+1:iEnd]}?> ")
+        madLibs.addToOutput(tag[0:iStart] + word + tag[iEnd+1:])
+        tag = madLibs.nextTag()
+    print(madLibs._outputText)
