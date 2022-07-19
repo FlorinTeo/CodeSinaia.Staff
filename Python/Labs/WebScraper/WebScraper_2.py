3d4f39f1efcef81340c4b4edb37432a2673c1f75
Florin.Teo <Florin.Teo@live.com>
2022-07-19 11:47:33 +0300

    Add WebScraper_1&2

--- Python/Labs/WebScraper/WebScraper_2.py
+++ Python/Labs/WebScraper/WebScraper_2.py
@@ -7,17 +7,17 @@ class WebScraper:
     # _url: URL to be scraped
     # _filename: filename to be used to save the scraped words
     # _words: words extracted from the URL page
-    # _nWords: total number of words extracted
+    # _iWords: current word in the array _words
     # _scraped: boolean indicating whether the page was scraped
 
     ###
     # Class constructor. Retains internally the URL and resets the state fields:
     # No words, count is 0, page had not been scraped yet.
-    def __init__(self, url, filename):
+    def __init__(self, url, filename = None):
         self._url = url
         self._filename = filename
-        self._words = ""
-        self._nWords = 0
+        self._words = []
+        self._iWord = 0
         self._scraped = False
         self._saved = False
 
@@ -28,31 +28,42 @@ class WebScraper:
         return True if len(word) == 5 and word.isalpha() else False
 
     ###
-    # Extracts and all words containing only the wordle words (5 letters only, a-z & A-Z only)
-    # each word is captialized and placed on a separate line
-    def scrape(self):
-        if self._scraped:
-            raise Exception("#Error#: Web page already scraped!")
+    # Initializes the iterator:
+    # If the webpage was scraped previously, just reset the current _iWord to 0
+    # Otherwise scrape the page for the first time and filter the wordle words and and store them in the internal array _words
+    def __iter__(self):
+        if not self._scraped:
+            lines = requests.get(self._url).text.splitlines()
+            for line in lines:
+                words = line.split()
+                for word in words:
+                    if WebScraper.isWordleWord(word):
+                        self._words.append(word.upper())
+            self._scraped = True
+        self._iWord = 0
+        return self
 
-        lines = requests.get(self._url).text.splitlines()
-        for line in lines:
-            words = line.split()
-            for word in words:
-                if WebScraper.isWordleWord(word):
-                    self._words += f"{word.upper()}\n"
-                    self._nWords += 1
-        self._scraped = True
+    ###
+    # Moves the iterator to the next word in the internal array _words, if available
+    def __next__(self):
+        if self._iWord == len(self._words):
+            raise StopIteration
+        word = self._words[self._iWord]
+        self._iWord += 1
+        return word
 
     ###
     # Saves the scraped content to the previously given _filename on the local disk
     def save(self):
+        if self._filename == None:
+            raise Exception("#Error#: No filename provided!")
         if not self._scraped:
             raise Exception("#Error#: Web page NOT scraped yet!")
         if self._saved:
             raise Exception("#Error#: Web page already saved!")
         
         file = open(self._filename, "w")
-        file.write(self._words)
+        for word in self._words: file.write(word + '\n')
         self._saved = True
 
     ###
@@ -61,11 +72,13 @@ class WebScraper:
         if not self._scraped:
             output = f"Page {self._url} not scraped yet!"
         else:
-            output = f"{self._nWords} wordle words scraped from page {self._url}"
-            if not self._saved:
+            output = f"{len(self._words)} wordle words scraped from page {self._url}"
+            if self._saved:
+                output += f"\nFile {self._filename} of size {os.path.getsize(self._filename)} saved to disk."
+            elif self._filename != None:
                 output += f"\nFile {self._filename} not saved yet."
             else:
-                output += f"\nFile {self._filename} of size {os.path.getsize(self._filename)} saved to disk."
+                output += f"\nNo file saving requested."
         return output
 
 # Tests the PlayMadLibs function on the simplest MadLibs text
@@ -73,7 +86,8 @@ if __name__ == "__main__":
     # print the resulting text
     webScraper = WebScraper("https://www.gutenberg.org/files/2701/2701-0.txt", "MobbyDick_words.txt")
     print(webScraper)
-    webScraper.scrape()
-    print(webScraper)
+    # print 
+    for word in webScraper:
+        print(word)
     webScraper.save()
     print(webScraper)
