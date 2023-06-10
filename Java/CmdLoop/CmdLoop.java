89595b13b57e4d2d76fb5b11fb847fed8ee4bf4e
Florin.Teo <Florin.Teo@live.com>
2023-06-10 10:46:25 -0700

    Cleanup CmdLoop demo

--- Java/CmdLoop/CmdLoop.java
+++ Java/CmdLoop/CmdLoop.java
@@ -3,30 +3,37 @@ package Java.CmdLoop;
 import java.util.Scanner; 
 
 public class CmdLoop { 
+
+    private static void printHelp() {
+        System.out.println("Available commands:");
+        System.out.println("help|?        : this help content.");
+        System.out.println("quit|exit|q|x : exits the command loop.");
+    }
+
     public static void main(String[] args) {
-        System.out.println("This is wordle help..."); 
-        System.out.print("Command?> ");
-        Scanner input = new Scanner(System.in);
-        String line = input.nextLine();
-    
-        while(!line.equalsIgnoreCase("exit") && !line.equalsIgnoreCase("quit")) 
-        {
-            if (line.isEmpty()) {
-                switch(line) {
-                    case "add":
-                        System.out.println("Processing add");
-                        break;
-                    case "match":
-                        System.out.println("Processing match");
-                        break;
-                    default:
-                        System.out.println("Command unrecognized");
-                }
+        System.out.println("This is a typical command loop..."); 
+  
+        Scanner console = new Scanner(System.in);
+        boolean done = false;
+
+        while (!done) {
+            System.out.print("Command [help] ?> ");
+            String line = console.nextLine();
+            switch(line.toLowerCase()) {
+                case "q":
+                case "x":
+                case "quit":
+                case "exit":
+                    done = true;
+                    break;
+                case "?":
+                case "help":
+                    printHelp();
+                    break;
             }
         }
-        System.out.println(line);
-        System.out.print("Command?> ");
-        line = input.nextLine();
+
+        System.out.println("Goodbye!");
+        console.close();
     }
-        
 }
