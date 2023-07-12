49e116523302998f0999403dc2fd6e942c30ac30
Florin.Teo <Florin.Teo@live.com>
2023-07-12 08:28:38 -0700

    Add some randomness

--- 2023/EightQueens/Program.java
+++ 2023/EightQueens/Program.java
@@ -14,19 +14,22 @@ public class Program {
     }
 
     private static boolean placeQueen(int nQueen) {
+        int rnd = (int)(Math.random() * 8);
         for (int r = 0; r < _board.length; r++) {
             for (int c = 0; c < _board[r].length; c++) {
-                if (_board[r][c] == 0) {
-                    updateBoard(r, c, 1);
+                int rR = (r + rnd) % 8;
+                int rC = (c + rnd) % 8;
+                if (_board[rR][rC] == 0) {
+                    updateBoard(rR, rC, 1);
                     if  (nQueen == 8 || placeQueen(nQueen+1)) {
                         // trick: as we're on a straight backgrack,
                         // switch this queen's position to it's counter
                         // as a negative number to disambiguate from the
                         // other markings when printing the board.
-                        _board[r][c] = -nQueen;
+                        _board[rR][rC] = -nQueen;
                         return true;
                     } else {
-                        updateBoard(r, c, -1);
+                        updateBoard(rR, rC, -1);
                     }
                 }
             }
