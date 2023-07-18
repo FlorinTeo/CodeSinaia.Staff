b3f66336df8556d8c6be1056430fc18735c87f25
Florin.Teo <Florin.Teo@live.com>
2023-07-18 07:53:27 +0200

    Add CodeDepth

---
+++ 2023/CodeDepth/Program.java
@@ -0,0 +1,46 @@
+package CodeDepth;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner console = new Scanner(System.in);
+        System.out.printf("Current folder: %s\n", (new File(".")).getAbsolutePath());
+        System.out.printf("Code file? > ");
+        String codeFilename = console.nextLine();
+
+        File codeFile = new File(codeFilename);
+        int codeDepth = calculateDepth(codeFile);
+        System.out.printf("Code depth of '%s' : %d\n", codeFile.getName(), codeDepth);
+
+        System.out.println("Goodbye!");
+        console.close();
+    }
+
+    private static int calculateDepth(File codeFile) throws FileNotFoundException {
+        Scanner codeReader = new Scanner(codeFile);
+
+        int crtDepth = 0;
+        int codeDepth = 0;
+        boolean inLiteral = false;
+        
+        while(codeReader.hasNextLine()) {
+            Scanner lineReader = new Scanner(codeReader.nextLine());
+            while(lineReader.hasNext()) {
+                String token = lineReader.next();
+                for(char ch : token.toCharArray()) {
+                    if (ch == '{') {
+                        crtDepth++;
+                        codeDepth = Math.max(crtDepth, codeDepth);
+                    } else if (ch == '}') {
+                        crtDepth--;
+                    }
+                }
+            }
+        }
+        codeReader.close();
+        return codeDepth;
+    }
+}
