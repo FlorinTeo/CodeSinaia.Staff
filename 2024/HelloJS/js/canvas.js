8d171f636e563a45d1279f1728c3fbaa2cf9bb85
Florin.Teo <Florin.Teo@live.com>
2024-02-24 14:08:16 -0800

    small touches

--- 2024/HelloJS/js/canvas.js
+++ 2024/HelloJS/js/canvas.js
@@ -5,11 +5,14 @@ export class Canvas {
     }
 
     resize(width, height) {
+        this.label = 'A';
         this.hElement.width = width;
         this.hElement.height = height;
         let context = this.hElement.getContext("2d");
         context.fillStyle = "white";
         context.fillRect(0, 0, width, height);
+        context.font = "bold 12px Arial";
+        context.textAlign = "center";
     }
 
     onMouseClickEvent(event) {
@@ -17,11 +20,14 @@ export class Canvas {
         const y = event.clientY - this.hElement.offsetTop;
         let context = this.hElement.getContext("2d");
         context.beginPath();
-        context.arc(x, y, 10, 0, 2 * Math.PI, false);
+        context.arc(x, y, 16, 0, 2 * Math.PI, false);
         context.fillStyle = 'lightgray';
         context.fill();
-        context.lineWidth = 2;
-        context.strokeStyle = 'red';
+        context.lineWidth = 1;
+        context.strokeStyle = 'black';
         context.stroke();
+        context.fillStyle = "red";
+        context.fillText(this.label, x, y+4);
+        this.label = String.fromCharCode(this.label.charCodeAt(0) + 1);
     }
 }
