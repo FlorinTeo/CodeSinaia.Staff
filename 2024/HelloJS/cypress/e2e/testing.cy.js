0bb1ab042a1a6805561a278cded64bb11c3511ef
Stefan Ene <enes@oregonstate.edu>
2024-03-21 20:07:38 -0700

    fiddeled with line test

--- 2024/HelloJS/cypress/e2e/testing.cy.js
+++ 2024/HelloJS/cypress/e2e/testing.cy.js
@@ -1,36 +1,43 @@
 
-// describe('Create a line', () => {
+describe('Create a line', () => {
+  it('passes', () => {
+    cy.visit("http://localhost:3001")
+
+    // create two nodes
+    cy.get('#hMainCanvas').click(100,100)
+    cy.get('#hMainCanvas').click(300,300)
+    
+
+    cy.get('#hMainCanvas')
+      .trigger('mousedown', 100, 100, { which: 0 })
+      .trigger('mousemove', 300, 300, { which: 1 })
+      .trigger('mouseup', 300, 300, { which: 1 })
+
+    // cy.get('#hMainCanvas').click(100,100, {release:false})
+    // cy.get('#hMainCanvas').click(300,300, {release:true})
+
+  })
+})
+
+// describe('Enqueue and Dequeue two nodes', () => {
 //   it('passes', () => {
 //     cy.visit("http://localhost:3001")
-
+    
 //     // create two nodes
 //     cy.get('#hMainCanvas').click(100,100)
 //     cy.get('#hMainCanvas').click(300,300)
 
-//     cy.contains("A").trigger("mousedown")
+//     cy.get('#hMainCanvas').rightclick(100,100)
+//     cy.contains("Enqueue").click()
+
+//     cy.get('#hMainCanvas').rightclick(300,300)
+//     cy.contains("Enqueue").click()
+
+//     cy.get('#hMainCanvas').rightclick(100,100)
+//     cy.contains("Dequeue").click()
+
+//     cy.get('#hMainCanvas').rightclick(300,300)
+//     cy.contains("Dequeue").click()
 
 //   })
 // })
-
-describe('Enqueue and Dequeue two nodes', () => {
-  it('passes', () => {
-    cy.visit("http://localhost:3001")
-    
-    // create two nodes
-    cy.get('#hMainCanvas').click(100,100)
-    cy.get('#hMainCanvas').click(300,300)
-
-    cy.get('#hMainCanvas').rightclick(100,100)
-    cy.contains("Enqueue").click()
-
-    cy.get('#hMainCanvas').rightclick(300,300)
-    cy.contains("Enqueue").click()
-
-    cy.get('#hMainCanvas').rightclick(100,100)
-    cy.contains("Dequeue").click()
-
-    cy.get('#hMainCanvas').rightclick(300,300)
-    cy.contains("Dequeue").click()
-
-  })
-})
